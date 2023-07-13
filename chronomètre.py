8fdb7a0054eda61396cf36e1c5922f29e43e48b0
Jonathan COURTOIS <Jonathan.courtois.pro@gmail.com>
2023-07-13 09:02:04 +0200

    Code pour le bouton 'lancer le chronomètre'

---
+++ chronomètre.py
@@ -0,0 +1,22 @@
+# import du module Tkinter pour la création d'une fenêtre
+from tkinter import *
+
+fenetrePrincipale = Tk()
+
+def getEntry():
+    print(ChampChoiceTime.get())
+    if (= True)
+    ResultatTime = Label(fenetrePrincipale, text = ChampChoiceTime.get()) 
+    ResultatTime.pack()
+
+fenetrePrincipale.geometry('500x500')
+fenetrePrincipale.resizable(0, 0)
+LabelChoiceTime = Label (fenetrePrincipale, text="Lancer le Chronométre")
+ChampChoiceTime = Entry (fenetrePrincipale)
+LaunchChrono = Button(fenetrePrincipale, text ="Lancer le Chronomètre", command=getEntry)
+
+LabelChoiceTime.pack()
+ChampChoiceTime.pack()
+LaunchChrono.pack()
+
+fenetrePrincipale.mainloop()
